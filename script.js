0b60635395119792a7a4c7f513f4e5142a6f3f70
slickGH <eccontact@anima-pictures.com>
2025-07-17 12:18:55 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let BACKEND_URL = 'https://3vqeh4z7ed.eu-west-1.awsapprunner.com';
+
+
+if (BACKEND_URL[BACKEND_URL.length - 1] === '/') {
+  BACKEND_URL = BACKEND_URL.slice(0, -1);
+}  
+
+setInterval(function () {
+  fetch(`${BACKEND_URL}/time`)
+    .then(response => response.json())
+    .then(data => {
+      const timer = document.getElementById('timer');
+      timer.innerHTML = data.time;
+    });
+}, 1000);
